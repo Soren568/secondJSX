f7a35e96992947a2e87ea97f0b7c96de5e87ffaa
Soren <85842462+Soren568@users.noreply.github.com>
2021-10-25 09:25:54 -0600

    change age

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 
 class PersonCard extends Component{
-render() {
-    return(
-        <>
-            <h1> {this.props.firstName} {this.props.lastName} </h1>
-            <p> Age: {this.props.age} </p>
-            <p> Hair color: {this.props.hair} </p>
-        </>
-    )
-}
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            age: this.props.age
+        }
+    };
+    incAge = () => {
+        // this.setState({message: "Here is a birthday"});
+        this.setState({ age: this.state.age + 1})
+    };
+    render() {
+        return(
+            <>
+                {/* <h1>{this.state.message}</h1> */}
+                <h1> {this.props.firstName} {this.props.lastName} </h1>
+                <p> Age: {this.state.age} </p>
+                <p> Hair color: {this.props.hair} </p>
+                <button onClick={this.incAge}> Birthday button</button>
+            </>
+        )
+    }
 }
 
 export default PersonCard;
